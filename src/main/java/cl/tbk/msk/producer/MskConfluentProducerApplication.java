ddce67f3caeea7a006271faa62cfddc8e4bffb16
Mauricio Villagran Mora <m.villagran.mora@accenture.com>
2023-07-20 17:09:49 -0400

    first commit

---
+++ src/main/java/cl/tbk/msk/producer/MskConfluentProducerApplication.java
@@ -0,0 +1,13 @@
+package cl.tbk.msk.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MskConfluentProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MskConfluentProducerApplication.class, args);
+	}
+
+}
